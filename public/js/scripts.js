83b202d412aaa505b3711de33e28d14610793e6e
girik-met-bu <girik@bu.edu>
2023-10-12 17:27:16 -0400

    Updated Back-End Code
    Updated Back-End Code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,67 @@
 function showNotImplementedMessage() {
     event.preventDefault();
     alert("File System Scanning feature is still in progress and has not been implemented yet. Please check back later for updates.");
- }
-
-var domain_name = "trivy.dev";
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const repositoryUrlInput = document.getElementById('githubUrl');
+    const generateReportBtn = document.getElementById('generateReportBtn');
+    generateReportBtn.disabled = true;
+    repositoryUrlInput.addEventListener('input', function () {
+        const isValidUrl = validateRepositoryUrl(repositoryUrlInput.value);
+        generateReportBtn.disabled = !isValidUrl;
+        generateReportBtn.value = isValidUrl ? 'Validate' : 'Submit';
+    });
+    function validateRepositoryUrl(url) {
+        const regex = /^(https?:\/\/)?(www\.)?github\.com\/[^\s\/]+\/[^\s\/]+$/;
+        return regex.test(url);
+    }
+    let isFormSubmitted = false;
+    document.getElementById('repositoryForm').addEventListener('submit', async function (e) {
+        if (!isFormSubmitted) {
+            e.preventDefault();
+            const repositoryUrl = repositoryUrlInput.value;
+            const isValidUrl = validateRepositoryUrl(repositoryUrl);
+            if (isValidUrl) {
+                await checkRepositoryExists(repositoryUrl);
+            } else {
+                alert('Invalid repository URL');
+            }
+        } else {
+            isFormSubmitted = false;
+        }
+    });
+    async function checkRepositoryExists(repositoryUrl) {
+        const strippedUrl = repositoryUrl.replace(/^(https?:\/\/)?(www\.)?github\.com\//, '');
+        const apiUrl = `https://api.github.com/repos/${strippedUrl}`;
+        try {
+            const response = await fetch(apiUrl);
+            if (response.ok) {
+                generateReportBtn.disabled = false;
+                generateReportBtn.value = 'Submit';
+                isFormSubmitted = false;
+                alert('The entered repository exist, please proceed further');
+                window.location.href = '/scan_results';
+                const response = await fetch('/validate-repo', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        repoUrl: repositoryUrl,
+                    }),
+                });
+                if (response.ok) {
+                    const result = await response.json();
+                    console.log('Validation successful:', result);
+                }
+            } else {
+                console.log('Repository does not exist:', repositoryUrl);
+                generateReportBtn.value = 'Validate';
+                alert('The repository does not exist.');
+            }
+        } catch (error) {
+            console.error('Error checking repository:', error);
+            // alert('An error occurred while checking the repository. Please try again later.');
+        }
+    }
+});
--- server.js
+++ server.js
@@ -1,17 +1,43 @@
-const express = require('express');
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 const path = require('path');
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 function handleRoute(req, res, fileName) {
   res.sendFile(path.join(__dirname, 'public', fileName));
 }
 
+app.post('/validate-repo', (req, res) => {
+  const { repoUrl } = req.body;
+    exec(`python3 trivy_detection.py --repo ${repoUrl}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing the script: ${error}`);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+    });
+});
+
+app.get('/check-url', (req, res) => {
+  const url = req.query.url;
+  const validUrl = require('valid-url');
+  if (validUrl.isUri(url) && url.startsWith('https://github.com/')) {
+      res.json({ message: 'Valid GitHub Repository URL' });
+  } else {
+      res.json({ message: 'Invalid GitHub Repository URL' });
+  }
+});
+
 app.get('/:page', (req, res) => {
   const page = req.params.page;
-  handleRoute(req, res, `${page}.html`);
+  if (page == 'validate-repo') {
+    console.log(page);
+  } else {
+    handleRoute(req, res, `${page}.html`);
+  }  
 });
 
 app.listen(PORT, () => {
